5e2726cc97a8bc4879ae9137cfb2c770ef483603
Tristoung2 <115805242+Tristoung2@users.noreply.github.com>
2024-01-26 13:13:55 +0100

    changement de html carré carré

--- public/src/scenes/MenuScene.js
+++ public/src/scenes/MenuScene.js
@@ -4,27 +4,49 @@ export class MenuScene extends Scene {
     constructor() {
         super({ 
             key: 'MenuScene',
-         });
+        });
     }
 
     preload() {
         this.load.image('backgroundMenu', 'assets/images/background.jpg');
+        this.load.image('logo', 'assets/images/logo.png');
+        
         this.load.html('nameform', 'assets/html_elements/home_menu.html');
-
+        this.load.html('connectedform', 'assets/html_elements/connected_menu.html');
     }
 
     create() {
         const background = this.add.image(this.scale.width / 2, this.scale.height / 2, 'backgroundMenu');
+        const logo = this.add.image(this.scale.width / 2, this.scale.height / 3, 'logo');
 
-        const htmlContent = this.cache.html.get('nameform');
+        this.showNameForm();
 
-        const element = this.add.dom(this.scale.width / 2, this.scale.height / 2).createFromHTML(htmlContent);
-
-        const loginButton = element.getChildByID('loginButton');
+        const loginButton = this.getElementByID('loginButton');
         loginButton.addEventListener('click', () => {
-            const username = document.getElementById('username').value;
+            const username = this.getElementValueByID('username');
             console.log("Votre pseudo : " + username);
-        });
 
+            this.showConnectedForm();
+        });
+    }
+
+    showNameForm() {
+        const htmlContent = this.cache.html.get('nameform');
+        this.currentForm = this.add.dom(this.scale.width / 2, this.scale.height / 1.5).createFromHTML(htmlContent);
+    }
+
+    showConnectedForm() {
+        this.currentForm.destroy();
+
+        const connectedHtmlContent = this.cache.html.get('connectedform');
+        this.currentForm = this.add.dom(this.scale.width / 2, this.scale.height / 1.5).createFromHTML(connectedHtmlContent);
+    }
+
+    getElementByID(elementID) {
+        return this.currentForm.getChildByID(elementID);
+    }
+
+    getElementValueByID(elementID) {
+        return this.getElementByID(elementID).value;
     }
 }
